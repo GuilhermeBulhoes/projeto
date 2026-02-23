d483561637220bd8326f479c44dad858b45a66c0
Guilherme Bulhoes <g.bulhoes.17@gmail.com>
2026-02-23 01:29:42 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,14 @@
+function toggLeMode () {
+  const html = document.documentElement
+  html.classList.toggLeMode('light')
+  // pegar a tag Img 
+  const img = document.querySelector("profile img")
+  // substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/assets/assets/avatar-light.png')
+  } else { 
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/assets/assets/avatar.png')
+  }
+}
